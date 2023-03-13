96c711219b1baa7fbea4b6375da49b2ab5974d75
iamnothalim <78136719+iamnothalim@users.noreply.github.com>
2023-03-13 17:50:53 +0900

    update draft version of Staking Contract

--- contracts/ThePos.sol
+++ contracts/ThePos.sol
@@ -1,10 +1,17 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ThePos is ERC20 {
-    constructor() ERC20("ThePos", "TPOS") {
-        _mint(msg.sender, 12000000000 * 10 ** decimals());
+contract ThePos is ERC20, Ownable {
+    constructor() ERC20("ThePos", "TPOS") {}
+
+    function setContractToOwner(address _thePosStakingContract) external {
+        transferOwnership(_thePosStakingContract);
+    }
+    
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
     }
 }
